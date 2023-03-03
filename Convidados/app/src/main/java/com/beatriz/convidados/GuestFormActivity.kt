d3385e9a9feb05f19ab33e913233105e9063d8ae
Beatrizvn <beatriz-vn@hotmail.com>
2023-03-03 17:28:44 -0300

    Cadastro Activity

--- Convidados/app/src/main/java/com/beatriz/convidados/GuestFormActivity.kt
+++ Convidados/app/src/main/java/com/beatriz/convidados/GuestFormActivity.kt
@@ -2,10 +2,29 @@ package com.beatriz.convidados
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.ViewModelProvider
+import com.beatriz.convidados.databinding.ActivityGuestFormBinding
 
-class GuestFormActivity : AppCompatActivity() {
+class GuestFormActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityGuestFormBinding
+    private lateinit var viewModel: GuestFormViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_guest_form)
+
+        binding = ActivityGuestFormBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        viewModel = ViewModelProvider(this).get(GuestFormViewModel::class.java)
+
+        binding.buttonSave.setOnClickListener(this)
+        binding.radioPresence.isChecked = true
+    }
+
+    override fun onClick(v: View) {
+        if(v.id == R.id.button_save){
+
+        }
     }
 }
---
+++ Convidados/app/src/main/java/com/beatriz/convidados/GuestFormViewModel.kt
@@ -0,0 +1,8 @@
+package com.beatriz.convidados
+
+import androidx.lifecycle.ViewModel
+
+class GuestFormViewModel: ViewModel() {
+
+
+}
